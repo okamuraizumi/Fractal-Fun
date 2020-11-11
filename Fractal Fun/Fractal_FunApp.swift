50f13889b68525182c8fc270120f021619273014
Izumi Okamura <okamuraizumi@gmail.com>
2020-11-10 19:57:32 -0700

    Initial Commit

---
+++ Fractal Fun/Fractal_FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fractal_FunApp.swift
+//  Fractal Fun
+//
+//  Created by Izumi Okamura on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Fractal_FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fractal FunTests/Fractal_FunTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fractal_FunTests.swift
+//  Fractal FunTests
+//
+//  Created by Izumi Okamura on 11/10/20.
+//
+
+import XCTest
+@testable import Fractal_Fun
+
+class Fractal_FunTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
