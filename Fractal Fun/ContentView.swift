ad5e5953cf2629ed828c09ebd7440e2fc7cc7faa
Izumi Okamura <okamuraizumi@gmail.com>
2020-12-02 20:32:35 -0700

    first part of success
    git commit
    q

--- Fractal Fun/ContentView.swift
+++ Fractal Fun/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sliderOneValue=0.0
+
+    @State var sliderTwoValue=0.0
+    
+    @State var alertIsVisible=false
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Button(action: {self.alertIsVisible = true}) {
+                Text("Push me")
+            }
+            .alert(isPresented: $alertIsVisible) {()-> Alert in
+                return Alert(title: Text("FRACTAL"),
+                             message: Text("slider one value is \(getSliderOneValue())\n" +
+                             "slider two value is \(getSliderTwoValue())")
+                )
+            }
+            Slider(value: $sliderOneValue, in:-2.0...2.0)
+            Slider(value: $sliderTwoValue, in:-2.0...2.0)
+        Text("We <3 Math")
             .padding()
+            
+        }
+    }
+    func getSliderOneValue() ->Double{
+        return sliderOneValue
+        
+        
+    }
+    func getSliderTwoValue() ->Double{
+        return sliderTwoValue
     }
 }
 
@@ -18,4 +47,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func getUpdatedValues(x:Double,y:Double)-> (x_new:Double,y_new:Double) {
+   let x_new=(x*x-y*y)
+   let y_new=(2*x*y)
+    return (x_new, y_new)
+
 }
