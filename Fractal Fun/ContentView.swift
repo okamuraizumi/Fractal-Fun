5be0189ea04eda4e47ee285de0d031074c8f0621
Orenda <orenda.williams@addepar.com>
2020-12-07 20:32:51 -0700

    BOOM

--- Fractal Fun/ContentView.swift
+++ Fractal Fun/ContentView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+
+var increment=1.0
+
 struct ContentView: View {
     @State var sliderOneValue=0.0
 
@@ -14,16 +17,27 @@ struct ContentView: View {
     
     @State var alertIsVisible=false
     
+    @State var x=0.0
+    @State var y=0.0
+    @State var xNew=0.0
+    @State var yNew=0.0
     
+    @State var my2DArray = Array(stride(from: -2.0, to: increment + 2.0, by: increment))
+    let zeroedArray = Array(repeating: 0, count: 7)
+        
     var body: some View {
         VStack {
-            Button(action: {self.alertIsVisible = true}) {
+            Button(action: {self.alertIsVisible = true;
+                    x=getSliderOneValue();
+                    y=getSliderTwoValue();
+                    (xNew,yNew)=getUpdatedValues(x:x,y:y)
+            }) {
                 Text("Push me")
             }
             .alert(isPresented: $alertIsVisible) {()-> Alert in
                 return Alert(title: Text("FRACTAL"),
-                             message: Text("slider one value is \(getSliderOneValue())\n" +
-                             "slider two value is \(getSliderTwoValue())")
+                             message: Text( "new x value is \(my2DArray)\n" +
+                                           "new y value is \(zeroedArray)\n" )
                 )
             }
             Slider(value: $sliderOneValue, in:-2.0...2.0)
@@ -55,3 +69,5 @@ func getUpdatedValues(x:Double,y:Double)-> (x_new:Double,y_new:Double) {
     return (x_new, y_new)
 
 }
+
+//func getChartPointArray(xMin:Double,xMax:Double,yMin:Double,yMax:Double,increment:Double)-> (Array<Array<Double>>){}
