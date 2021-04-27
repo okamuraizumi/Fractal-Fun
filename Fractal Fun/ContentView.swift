2d1e4f02f51002ff2c8c4b782a3d7f4268028f73
Orenda <orenda.williams@addepar.com>
2021-04-26 21:19:23 -0600

    we have a circle

--- Fractal Fun/ContentView.swift
+++ Fractal Fun/ContentView.swift
@@ -60,6 +60,7 @@ struct BagOSquares: View {
     var scale: CGFloat = 50
     var rows: [GridItem] =
         Array(repeating: .init(.flexible(minimum: 50), spacing: 0.0, alignment: .topLeading), count: 3)
+    
     var body: some View {
         let square = Square(coordinate: (0,0), scale: scale, color: .red);
         let square2 = Square(coordinate: (0,0), scale: scale, color: .blue);
@@ -77,7 +78,7 @@ struct BagOSquares: View {
             square6.fill(square6.color).padding(0.0)
         }
     
-    
+
     }
     
    
@@ -94,13 +95,13 @@ struct ContentView: View {
     @State var sliderOneValue=1.0
 
     @State var sliderTwoValue=1.0
-    @State var sliderThreeValue=2.0
+    @State var sliderThreeValue=10.0
     
     @State var alertIsVisible=false
     
     @State var x=0.0
     @State var y=0.0
-    @State var numOfPoints=0
+    @State var numOfPoints=10
     @State var xNew=0.0
     @State var yNew=0.0
     
@@ -108,11 +109,13 @@ struct ContentView: View {
     @State var coordinates = [(Double, Double)]()
     @State var newCoordinates = [(Double, Double)]()
     @State var inSetCoordinate = [Bool]()
+    @State var colorCoordinates = [UInt32]()
+    
     
     @State var bagOSquares=BagOSquares()
     
     init(){
-        fillPointsArray(xLower: -2.0, xUpper: 2.0, xInNum: 5, yLower: -2.0, yUpper: 2.0, yInNum: 5)
+        fillPointsArray(xLower: -2.0, xUpper: 2.0, xInNum: numOfPoints, yLower: -2.0, yUpper: 2.0, yInNum: numOfPoints)
         
     }
     
@@ -145,19 +148,25 @@ struct ContentView: View {
 //                spiroSquare.spiroSquare.updatePoints(inSetCoordinate, newCoordinates)
 //                self.view
 //                self.viewToReload.layoutIfNeeded()
-                    print(newCoordinates);
-                    print(inSetCoordinate);
+//                    print(newCoordinates);
+//                    print(inSetCoordinate);
             }) {
                 Text("New Values")
             }
 
             Slider(value: $sliderOneValue, in:1.0...3.0)
             Slider(value: $sliderTwoValue, in:1.0...3.0)
-            Slider(value: $sliderThreeValue, in:2.0...10.0)
+            Slider(value: $sliderThreeValue, in:10.0...100.0)
             
             Text("We <3 Math\(numOfPoints)").padding();
             
-            bagOSquares.frame(width:50, height: 50)
+            var imageCreated = makeImageWithColor(colorCoordinates, numOfPoints: numOfPoints);
+            Image(uiImage:imageCreated);
+//            UIImage(imageCreated);
+//            let imageView = UIImageView(image: imageCreated);
+//            imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100);
+//            self.view.addSubview(imageView);
+//            bagOSquares.frame(width:50, height: 50)
             
 //            bagOSquares.square
 //                 //.stroke()
@@ -194,11 +203,12 @@ struct ContentView: View {
         let yIncrement=(yUpper-yLower)/Double(yInNum-1)
         for indX in 0...xInNum-1 {
             for indY in 0...yInNum-1{
-                xCoor=xLower+Double(indX)*xIncrement
-                yCoor=yLower+Double(indY)*yIncrement
-                coordinates.append((xCoor,yCoor))
-                newCoordinates.append((xCoor,yCoor))
-                inSetCoordinate.append(true)
+                xCoor=xLower+Double(indX)*xIncrement;
+                yCoor=yLower+Double(indY)*yIncrement;
+                coordinates.append((xCoor,yCoor));
+                newCoordinates.append((xCoor,yCoor));
+                inSetCoordinate.append(true);
+                colorCoordinates.append(0xFF8803fc);
             }
         }
         
@@ -207,13 +217,20 @@ struct ContentView: View {
         var Xcoordinate:Double
         var Ycoordinate:Double
         var coordinate:(Double,Double)
+        var newPoints:(Double,Double)
+        var newXcoordinate:Double
+        var newYcoordinate:Double
         for indexCoordinate in 0...newCoordinates.count-1{
             coordinate = newCoordinates[indexCoordinate];
             Xcoordinate=coordinate.0;
             Ycoordinate=coordinate.1;
-            newCoordinates[indexCoordinate]=getUpdatedValues(x:Xcoordinate, y:Ycoordinate);
-            if( (Xcoordinate*Xcoordinate + Ycoordinate*Ycoordinate) > 4 ){
+            newPoints = getUpdatedValues(x:Xcoordinate, y:Ycoordinate);
+            newXcoordinate = newPoints.0;
+            newYcoordinate = newPoints.1;
+            newCoordinates[indexCoordinate]=newPoints;
+            if( (newXcoordinate*newXcoordinate + newYcoordinate*newYcoordinate) > 4 ){
                 inSetCoordinate[indexCoordinate]=false;
+                colorCoordinates[indexCoordinate]=0xFFfcba03;
             }
         }
     }
@@ -241,6 +258,39 @@ func getColor (pixelValue:Bool) -> UIColor{
     else{
         return .red
     }
+}
+
+func makeImageWithColor(_ colors: [UInt32], numOfPoints: Int) -> UIImage {
+//    let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+//    UIGraphicsBeginImageContextWithOptions(size, false, 0)
+//    color.setFill()
+//    UIRectFill(rect)
+//    let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+//    UIGraphicsEndImageContext()
+//    return image)
+    
+//    var srgbArray = [UInt32](repeating: 0xFFfcba03, count: 100)
+//
+//    for index in 1...50{
+//        srgbArray.append(contentsOf: [UInt32](repeating: 0xFF8803fc, count:100));
+//        srgbArray.append(contentsOf: [UInt32](repeating: 0xFFfcba03, count:100));
+//    }
+    
+    var colorsArray = colors;
+        
+    let cgImg = colorsArray.withUnsafeMutableBytes { (ptr) -> CGImage in
+        let ctx = CGContext(
+            data: ptr.baseAddress,
+            width: numOfPoints,
+            height: numOfPoints,
+            bitsPerComponent: 8,
+            bytesPerRow: 4*numOfPoints,
+            space: CGColorSpace(name: CGColorSpace.sRGB)!,
+            bitmapInfo: CGBitmapInfo.byteOrder32Little.rawValue + CGImageAlphaInfo.premultipliedFirst.rawValue
+        )!
+        return ctx.makeImage()!
+    }
+    return UIImage(cgImage:cgImg);
 }
 
 //func getChartPointArray(xMin:Double,xMax:Double,yMin:Double,yMax:Double,increment:Double)-> (Array<Array<Double>>){}
